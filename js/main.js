7ab8f10619a3c39e6702adccb1a48d8b762b8661
Fabio Jr Abucejo <gtxon3k@gmail.com>
2025-01-25 00:49:17 +0800

    Added initial HTML structure

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+//Get current year for copyright statement-- >
+//        <!--$('#spanYear').html(new Date().getFullYear()); -->
+//const d = new Date();
+//    document.getElementById("#spanYear").innerHTML = d.getFullYear();
